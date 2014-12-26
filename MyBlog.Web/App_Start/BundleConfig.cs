ae94ef2c800bdc51b1e68c0a4938389df127fe6f
Mohsen Esmailpour <mo.esmp@gmail.com>
2014-12-26 14:41:21 +0330

    Admin area added.

--- MyBlog.Web/App_Start/BundleConfig.cs
+++ MyBlog.Web/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace MyBlog.Web
 {
@@ -23,13 +22,29 @@ namespace MyBlog.Web
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/metrojs").Include(
+                "~/Areas/MemberArea/Scripts/jquery.widget.min.js",
+                "~/Areas/MemberArea/Scripts/metro.min.js",
+                "~/Areas/MemberArea/Scripts/metro/metro-global.js",
+                "~/Areas/MemberArea/Scripts/metro/metro-core.js",
+                "~/Areas/MemberArea/Scripts/metro/metro-locale.js",
+                "~/Areas/MemberArea/Scripts/metro/metro-touch-handler.js",
+                "~/Areas/MemberArea/Scripts/metro/metro-dropdown.js",
+                "~/Areas/MemberArea/Scripts/metro/metro-input-control.js",
+                "~/Areas/MemberArea/Scripts/metro/metro-initiator.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
 
+            bundles.Add(new StyleBundle("~/content/admincss").Include(
+                      "~/content/admin/metro-bootstrap.css",
+                      "~/content/admin/metro-bootstrap-responsive.css",
+                      "~/content/admin/iconfont.css"));
+
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
-            BundleTable.EnableOptimizations = true;
+            BundleTable.EnableOptimizations = false;
         }
     }
 }
