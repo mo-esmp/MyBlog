c548b9713f9dade7af9a306e022ab26d4faba5c1
Mohsen Esmailpour <mo.esmp@hotmail.com>
2018-07-29 14:05:09 +0430

    Remove old project.

--- src/MyBlog.Service/ApplicationKey.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MyBlog.Service
-{
-    public class ApplicationKey
-    {
-        public const string Post = "Post-";
-        public const string Posts = "Post";
-        public const string Tags = "Tags";
-    }
-}
--- src/MyBlog.Web.Old/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyBlog.Web.MvcApplication" Language="C#" %>
